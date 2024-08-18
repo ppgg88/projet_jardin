c44e3c66b82cbe6f130a0537586ec19188d477b0
paul giroux <paul.giroux87@gmail.com>
2024-08-18 15:36:14 +0200

    test reception

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-import serial.tools.list_ports
+import serial
 
-ports = serial.tools.list_ports.comports()
-for port, desc, hwid in sorted(ports):
-    print("{}: {} [{}]".format(port, desc, hwid)
-        
-```
-This will list all the available serial ports on your system. You can run this script to see if the port is available or not.
-If you are using a Linux system, you can use the `ls /dev/tty*` command to list all the available serial ports.
-If you are using a Windows system, you can use the Device Manager to check the available serial ports.
-Once you have identified the correct port, you can update your Python script to use that port.
-```
+ser = serial.Serial('/dev/ttyUSB0', 115200)
+
+while True:
+    if ser.in_waiting > 0:
+        print(ser.readline().decode('utf-8').strip())
+
+ser.close()
