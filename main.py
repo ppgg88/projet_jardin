a90f7d7f123877ce14a489849d67367712628be6
paul giroux <paul.giroux87@gmail.com>
2024-08-18 22:17:59 +0200

    amelioration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import serial
 
+
 ser = serial.Serial('/dev/ttyUSB0', 115200)
 
 #S1
@@ -21,9 +22,38 @@ out_temp = 0
 out_humidity = 0
 
 def display_data():
-    print(f'S1: Time: {time} Temperture: {temperture} Humidity: {humidity} Luminosity: {luminosity}')
-    print(f'S2: Time: {pluvio_time} Wind: {wind} Pression: {pression} Soil Temp: {soil_temp}')
-    print(f'S3: Soil Humidity: {soil_humidity} Girus: {girus} Out Temp: {out_temp} Out Humidity: {out_humidity}')
+    print('-'*50)
+    print(f'Time: {time}sec')
+    print(f'Temperture: {temperture}°C')
+    print(f'Humidity: {humidity}%')
+    print(f'Luminosity: {luminosity}')
+    print(f'Pluvio Time: {pluvio_time}sec')
+    print(f'Wind: {wind}')
+    print(f'Pression: {pression}')
+    print(f'Soil Temp: {soil_temp}')
+    print(f'Soil Humidity: {soil_humidity}')
+    print(f'Girus: {girus}')
+    print(f'Out Temp: {out_temp}')
+    print(f'Out Humidity: {out_humidity}')
+
+
+def treat_data():
+    time = float(time)
+    temperture = float(temperture)
+    humidity = float(humidity)
+    luminosity = float(luminosity)
+
+    pluvio_time = float(pluvio_time)
+    wind = float(wind)
+    pression = float(pression)
+    soil_temp = float(soil_temp)
+
+    soil_humidity = float(soil_humidity)
+    girus = float(girus)
+    out_temp = float(out_temp)
+    out_humidity = float(out_humidity)
+
+
 
 while True:
     if ser.in_waiting > 0:
@@ -32,23 +62,24 @@ while True:
             data = data.decode('utf-8').strip()
             if len(data) and data[0] == 'S' and data[-1] == 'E':
                 if data[1] == '1':
-                    data = data[2:-1].split(';')
+                    data = data[3:-1].split(';')
                     time = data[0]
                     temperture = data[1]
                     humidity = data[2]
                     luminosity = data[3]
                 elif data[1] == '2':
-                    data = data[2:-1].split(';')
+                    data = data[3:-1].split(';')
                     pluvio_time = data[0]
                     wind = data[1]
                     pression = data[2]
                     soil_temp = data[3]
                 elif data[1] == '3':
-                    data = data[2:-1].split(';')
+                    data = data[3:-1].split(';')
                     soil_humidity = data[0]
                     girus = data[1]
                     out_temp = data[2]
                     out_humidity = data[3]
+                treat_data()
                 display_data()
         except UnicodeDecodeError:
             pass
