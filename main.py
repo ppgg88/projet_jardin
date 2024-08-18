cf8e7ff88f3135714838706d8637986414e24f10
paul giroux <paul.giroux87@gmail.com>
2024-08-18 21:35:53 +0200

    debug

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ ser = serial.Serial('/dev/ttyUSB0', 115200)
 
 while True:
     if ser.in_waiting > 0:
-        print(ser.readline().decode('utf-8').strip())
+        print(ser.readline())
 
 ser.close()
